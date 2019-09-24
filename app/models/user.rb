ce1314f956696dd0c9718119955b0b822be21d35
Rashad <rashad.walcott15@gmail.com>
2019-09-24 12:27:34 -0400

    Edit dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :username
-  has_many :favorites
+  has_many :favorites,dependent: :destroy
   has_many :breweries, through: :favorites
 end
